bdcbaa147ae86f4e4c96f672f97faafeabe19227
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-03-02 08:46:58 -0500

    Variables

--- giraffe.rb
+++ giraffe.rb
@@ -1,5 +1,19 @@
+################################
 # print "Hello World"
-puts "   /|"
-puts "  / |"
-puts " /  |"
-puts "/___|"
+
+#### Draw a shape #######
+
+#puts "   /|"
+#puts "  / |"
+#puts " /  |"
+#puts "/___|"
+
+### VARIABLES ###
+
+character_name = "Jhon"
+character_age = "15"
+
+puts ("There once was  a man named " + character_name)
+puts ("he was "+ character_age + " years old.")
+puts ("He really liked the name "+ character_name)
+puts ("but didn't like being " + character_age)
