c7785bac9a7f65b6d650b3b57f9d43937fbe595d
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-03-02 10:10:40 -0500

    Basics finished

--- ruby-basics.rb
+++ ruby-basics.rb
@@ -1,15 +1,16 @@
 ################################
-# print "Hello World"
+print "Hello World"
 
 #### Draw a shape #######
-
-#puts "   /|"
-#puts "  / |"
-#puts " /  |"
-#puts "/___|"
+puts "DRAW ASHAPE"
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
 
 ### VARIABLES ###
-=begin
+puts "VARIABLES"
+
 character_name = "Jhon"
 character_age = "15"
 
@@ -17,60 +18,65 @@ puts ("There once was  a man named " + character_name)
 puts ("he was "+ character_age + " years old.")
 puts ("He really liked the name "+ character_name)
 puts ("but didn't like being " + character_age)
-=end
+
 
 ### Data type ####
 
+puts "DATA TYPE"
 #String:
-#name = "Mike"
+name = "Mike"
 # number (as integer)
-#age = 75
+age = 75
 # float
-#gpa = 3.2564
+gpa = 3.2564
 #boolean
-#ismale = true
-#istall = false
+ismale = true
+istall = false
 # null (Doesn't have a value)
-#flaw = nil
+flaw = nil
 
-#### Working with String ####
+#### Working with Strings ####
+puts "WORKING WITH STRINGS"
+
 # print quotation mark \"
-#puts " \" Karol"
+puts " \" Karol"
 # print new line \n
-#puts " \n Karol"
+puts " \n Karol"
 
 #Methods
-#phrase = "Giraffe Academy"
+phrase = "Giraffe Academy"
 #Upper Case
-#puts phrase.upcase()
-
+puts phrase.upcase()
 #Lower Case
-#puts phrase.downcase()
+puts phrase.downcase()
 
 #strip (Remove spaces)
-#phrase = "    Hello  "
-#puts phrase.strip()
+phrase = "    Hello  "
+puts phrase.strip()
 
 #Length
-#puts phrase.length()
+puts phrase.length()
 
 # Includes (validate if the string has the pattern)
-#phrase = "This is my name"
-#puts phrase.include? "This" #true
-#puts phrase.include? "age" #false
+phrase = "This is my name"
+puts phrase.include? "This" #true
+puts phrase.include? "age" #false
 
 #Manage positions as an array
-#puts phrase[0] #first position
-#puts phrase[3] #fourth position
+puts phrase[0] #first position
+puts phrase[3] #fourth position
 #range:
-#puts phrase[0,4] #from fist position to fourth position
+puts phrase[0,4] #from fist position to fourth position
 
 #get index
-#puts phrase.index("m")
-#puts phrase.index("name") #give the index where starts
+puts phrase.index("m")
+puts phrase.index("name") #give the index where starts
 
-=begin
+
 #Math and Numbers
+
+puts "MATH AND NUMBERS"
+
 puts 5
 puts 5 + 9 #sum
 puts 5 - 3 #substract
@@ -79,7 +85,8 @@ puts 4 * 3 # multiply
 puts 10 % 5 #mudulus
 puts 2 ** 3 # exponent
 
-#convert to String (.to_s)
+puts "convert to String (.to_s)"
+
 num = 20
 puts ("my favorite number is: " + num.to_s)
 
@@ -110,17 +117,20 @@ puts "integer with float got a float"
 puts 10 + 1
 puts 10.0 + 1.0
 puts 10 + 7.0
-=end
+
 
 ########################
 # Getting user input
 
+puts "GETTING USER INPUT"
+
 ## Install package platformio-ide-terminal
 ## Run ruby file $ ruby giraffe.rb
+
 puts "Enter your name: "
 name = gets  #With newline
 puts ("Hello " + name + ", you are cool")
 
 puts "Enter your name again: "
 name2 = gets.chomp() # No new newline
- puts ("Hello " + name2 + ", you are cool")
+puts ("Hello " + name2 + ", you are cool")
