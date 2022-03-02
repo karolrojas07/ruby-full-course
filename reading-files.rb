076b3e65249de7cb717e4c737fa4e74a5922bb80
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-03-02 18:32:24 -0500

    Finish Read files section

---
+++ reading-files.rb
@@ -0,0 +1,26 @@
+#  Open the file
+# r: read-only
+#
+File.open("employees.txt", "r") do |file|
+  puts file.read() ## Or file.readlines()
+end
+
+puts
+File.open("employees.txt", "r") do |file|
+  puts file.readline()
+  puts file.readline()
+end
+
+puts
+
+File.open("employees.txt", "r") do |file|
+  puts file.readchar()
+end
+
+puts
+
+File.open("employees.txt", "r") do |file|
+  for line in file.readlines()
+    puts line
+  end
+end
