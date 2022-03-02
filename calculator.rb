a2a532429e991eb16732f6c2402762bfba905149
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-03-02 10:23:20 -0500

    Calculator program

---
+++ calculator.rb
@@ -0,0 +1,14 @@
+puts "Enter a number: "
+num1 = gets.chomp()
+
+puts "Enter another number: "
+
+num2 = gets.chomp()
+
+puts ("Sum: " + num1 + " + "+ num2)
+puts "Integers"
+# Convert to integer (.to_i)
+puts (num1.to_i + num2.to_i)
+puts "Floats"
+# Convert to float (.to_f)
+puts (num1.to_f + num2.to_f)
