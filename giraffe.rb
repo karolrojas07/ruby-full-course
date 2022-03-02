1b7466a4c9bb680b98686c06d8682f6afb418474
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-03-02 08:52:37 -0500

    Data types

--- giraffe.rb
+++ giraffe.rb
@@ -9,7 +9,7 @@
 #puts "/___|"
 
 ### VARIABLES ###
-
+=begin
 character_name = "Jhon"
 character_age = "15"
 
@@ -17,3 +17,18 @@ puts ("There once was  a man named " + character_name)
 puts ("he was "+ character_age + " years old.")
 puts ("He really liked the name "+ character_name)
 puts ("but didn't like being " + character_age)
+=end
+
+### Data type ####
+
+#String:
+#name = "Mike"
+# number (as integer)
+#age = 75
+# float
+#gpa = 3.2564
+#boolean
+#ismale = true
+#istall = false
+# null (Doesn't have a value)
+#flaw = nil
