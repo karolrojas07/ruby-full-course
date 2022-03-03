793b9bc48437932d984985f4eaa6e4e561db2920
karolrojas07 <karol.rojasb.kommit@gmail.com>
2022-03-02 19:57:40 -0500

    Finish Inheritance section

--- classes-objects.rb
+++ classes-objects.rb
@@ -57,3 +57,33 @@ student2 = Student.new("Pam", "Art", 3.6)
 
 puts student1.has_honors
 puts student2.has_honors
+
+######### Inheritance
+class Chef
+  def make_chicken
+    puts "The chef makes chicken"
+  end
+  def make_salad
+    puts "The chef makes salad"
+  end
+  def make_special_dish
+    puts "The chef makes bbq ribs"
+  end
+end
+
+class ItalianChef < Chef
+  def make_special_dish
+    puts "The chef makes  eggplant parm"
+  end
+  def make_pasta
+    puts "The chef makes pasta"
+  end
+end
+
+chef = Chef.new()
+chef.make_chicken
+chef.make_special_dish
+
+italian_chef = ItalianChef.new()
+italian_chef.make_salad
+italian_chef.make_special_dish
